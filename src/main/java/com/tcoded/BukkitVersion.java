3fd60e50fd7c18be0060c8cf1d850b94dd2210b1
TechnicallyCoded <technicallycoded@gmail.com>
2024-07-13 16:46:34 +0200

    Initial Commit

---
+++ src/main/java/com/tcoded/BukkitVersion.java
@@ -0,0 +1,61 @@
+package com.tcoded;
+
+public enum BukkitVersion {
+
+    v1_8_R3(MCVersion.v1_8_8),
+    v1_9_R1(MCVersion.v1_9, MCVersion.v1_9_2),
+    v1_9_R2(MCVersion.v1_9_4),
+    v1_10_R1(MCVersion.v1_10_2),
+    v1_11_R1(MCVersion.v1_11, MCVersion.v1_11_2),
+    v1_12_R1(MCVersion.v1_12, MCVersion.v1_12_1, MCVersion.v1_12_2),
+    v1_13_R1(MCVersion.v1_13),
+    v1_13_R2(MCVersion.v1_13_1, MCVersion.v1_13_2),
+    v1_14_R1(MCVersion.v1_14, MCVersion.v1_14_1, MCVersion.v1_14_2, MCVersion.v1_14_3, MCVersion.v1_14_4),
+    v1_15_R1(MCVersion.v1_15, MCVersion.v1_15_1, MCVersion.v1_15_2),
+    v1_16_R1(MCVersion.v1_16_1),
+    v1_16_R2(MCVersion.v1_16_2, MCVersion.v1_16_3),
+    v1_16_R3(MCVersion.v1_16_4, MCVersion.v1_16_5),
+    v1_17_R1(MCVersion.v1_17, MCVersion.v1_17_1),
+    v1_18_R1(MCVersion.v1_18, MCVersion.v1_18_1),
+    v1_18_R2(MCVersion.v1_18_2),
+    v1_19_R1(MCVersion.v1_19, MCVersion.v1_19_1, MCVersion.v1_19_2),
+    v1_19_R2(MCVersion.v1_19_3),
+    v1_19_R3(MCVersion.v1_19_4),
+    v1_20_R1(MCVersion.v1_20, MCVersion.v1_20_1),
+    v1_20_R2(MCVersion.v1_20_2),
+    v1_20_R3(MCVersion.v1_20_4),
+    v1_20_R4(MCVersion.v1_20_5, MCVersion.v1_20_6),
+    v1_21_R1(MCVersion.v1_21)
+    ;
+
+    private final MCVersion[] mcVersions;
+
+    BukkitVersion(MCVersion... mcVersions) {
+        this.mcVersions = mcVersions;
+    }
+
+    public boolean greaterThan(BukkitVersion version) {
+        return this.ordinal() > version.ordinal();
+    }
+
+    public boolean greaterOrEqThan(BukkitVersion version) {
+        return this.ordinal() >= version.ordinal();
+    }
+
+    public boolean lessThan(BukkitVersion version) {
+        return this.ordinal() < version.ordinal();
+    }
+
+    public boolean lessOrEqThan(BukkitVersion version) {
+        return this.ordinal() <= version.ordinal();
+    }
+
+    public boolean isWithin(BukkitVersion versionLow, BukkitVersion versionHigh) {
+        return this.greaterOrEqThan(versionLow) && this.lessOrEqThan(versionHigh);
+    }
+
+    public MCVersion[] getMcVersions() {
+        return mcVersions;
+    }
+
+}
---
+++ src/main/java/com/tcoded/MCVersion.java
@@ -0,0 +1,109 @@
+package com.tcoded;
+
+import java.util.Objects;
+
+public enum MCVersion {
+
+    v1_8_8 (BukkitVersion.v1_8_R3),
+    v1_9 (BukkitVersion.v1_9_R1),
+    v1_9_1 (BukkitVersion.v1_9_R1),
+    v1_9_2 (BukkitVersion.v1_9_R1),
+//    v1_9_3 (BukkitVersion.v1_9_R1), // todo
+    v1_9_4 (BukkitVersion.v1_9_R2),
+    v1_10 (BukkitVersion.v1_10_R1),
+    v1_10_1 (BukkitVersion.v1_10_R1),
+    v1_10_2 (BukkitVersion.v1_10_R1),
+    v1_11 (BukkitVersion.v1_11_R1),
+    v1_11_1 (BukkitVersion.v1_11_R1),
+    v1_11_2 (BukkitVersion.v1_11_R1),
+    v1_12 (BukkitVersion.v1_12_R1),
+    v1_12_1 (BukkitVersion.v1_12_R1),
+    v1_12_2 (BukkitVersion.v1_12_R1),
+    v1_13 (BukkitVersion.v1_13_R1),
+    v1_13_1 (BukkitVersion.v1_13_R2),
+    v1_13_2 (BukkitVersion.v1_13_R2),
+    v1_14 (BukkitVersion.v1_14_R1),
+    v1_14_1 (BukkitVersion.v1_14_R1),
+    v1_14_2 (BukkitVersion.v1_14_R1),
+    v1_14_3 (BukkitVersion.v1_14_R1),
+    v1_14_4 (BukkitVersion.v1_14_R1),
+    v1_15 (BukkitVersion.v1_15_R1),
+    v1_15_1 (BukkitVersion.v1_15_R1),
+    v1_15_2 (BukkitVersion.v1_15_R1),
+    v1_16 (BukkitVersion.v1_16_R1),
+    v1_16_1 (BukkitVersion.v1_16_R1),
+    v1_16_2 (BukkitVersion.v1_16_R2),
+    v1_16_3 (BukkitVersion.v1_16_R2),
+    v1_16_4 (BukkitVersion.v1_16_R3),
+    v1_16_5 (BukkitVersion.v1_16_R3),
+    v1_17 (BukkitVersion.v1_17_R1),
+    v1_17_1 (BukkitVersion.v1_17_R1),
+    v1_18 (BukkitVersion.v1_18_R1),
+    v1_18_1 (BukkitVersion.v1_18_R1),
+    v1_18_2 (BukkitVersion.v1_18_R2),
+    v1_19 (BukkitVersion.v1_19_R1),
+    v1_19_1 (BukkitVersion.v1_19_R1),
+    v1_19_2 (BukkitVersion.v1_19_R1),
+    v1_19_3 (BukkitVersion.v1_19_R2),
+    v1_19_4 (BukkitVersion.v1_19_R3),
+    v1_20 (BukkitVersion.v1_20_R1),
+    v1_20_1 (BukkitVersion.v1_20_R1),
+    v1_20_2 (BukkitVersion.v1_20_R2),
+//    v1_20_3 (BukkitVersion.v1_20_R2), // todo
+    v1_20_4 (BukkitVersion.v1_20_R3),
+    v1_20_5 (BukkitVersion.v1_20_R4),
+    v1_20_6 (BukkitVersion.v1_20_R4),
+    v1_21 (BukkitVersion.v1_21_R1)
+    ;
+
+    public static MCVersion fromMcVersion(String mcVersion) {
+        Objects.requireNonNull(mcVersion, "mcVersion cannot be null");
+
+        String search = "v" + mcVersion.replace(".", "_");
+        for (MCVersion nmsVersion : values()) {
+            if (search.equals(nmsVersion.name())) {
+                return nmsVersion;
+            }
+        }
+
+        return null;
+    }
+
+    public static MCVersion fromServerVersion(String serverVersion) {
+        Objects.requireNonNull(serverVersion, "serverVersion cannot be null");
+
+        String mcVersionSection = serverVersion.split("-")[0];
+        return fromMcVersion(mcVersionSection);
+    }
+
+    private final BukkitVersion bukkitVer;
+
+    MCVersion(BukkitVersion bukkitVer) {
+        this.bukkitVer = bukkitVer;
+    }
+
+    public boolean greaterThan(MCVersion version) {
+        return this.ordinal() > version.ordinal();
+    }
+
+    public boolean greaterOrEqThan(MCVersion version) {
+        return this.ordinal() >= version.ordinal();
+    }
+
+    public boolean lessThan(MCVersion version) {
+        return this.ordinal() < version.ordinal();
+    }
+
+    public boolean lessOrEqThan(MCVersion version) {
+        return this.ordinal() <= version.ordinal();
+    }
+
+    public boolean isWithin(MCVersion versionLow, MCVersion versionHigh) {
+        return this.greaterOrEqThan(versionLow) && this.lessOrEqThan(versionHigh);
+    }
+
+    public BukkitVersion toBukkitVersion() {
+        return bukkitVer;
+    }
+
+}
